c8b8ef86cae37f83b6ec8e8e057b405606ce7393
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-26 17:20:35 -0300

    Add fibonacci implementation with memoization

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -10,6 +10,24 @@ def fibonacci(n):
     return fibonacci(n - 1) + fibonacci(n - 2)
 
 
+cache = dict()
+def dynamic_fibonacci(n):
+    """Recursively returns fibonacci sequence from n to 0.
+        Uses memoization for better performance.
+    Args:
+        n(int): number for sequence calculation.
+    Returns:
+        sequence sum.
+    """
+    if n <= 1:
+        return n
+
+    if not cache.get(n):
+        cache[n] = dynamic_fibonacci(n-1) + dynamic_fibonacci(n-2)
+
+    return cache[n]
+
 if __name__ == '__main__':
     print(fibonacci(10))
+    print(dynamic_fibonacci(10))
    
