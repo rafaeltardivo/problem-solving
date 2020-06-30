4d49d4d7e522ce3174c2a058a8d6e1089697ad67
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-30 19:45:40 -0300

    Add binary gap resolution

---
+++ binary_gap/binary_gap.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+def solution(number):
+    """Returns the greatest binary gap on the number.
+    ex: The number 529 has binary representation 1000010001 and contains 
+    two binary gaps: one of length 4 and one of length 3.
+
+    Complexity: O(n).
+
+    Args:
+        number (int): number to conver to be converted to binary.
+    Returns:
+        Length of the greatest gap or 0.
+    """
+    bin_repr = format(number, 'b')
+    size = len(bin_repr)
+    
+    for n in range(size - 1):
+        if bin_repr[n] == '1':
+            zeroes = bin_repr[n:].split('1')
+            if len(zeroes) > 2:
+                return (len(max(zeroes)))
+            break
+    return 0
