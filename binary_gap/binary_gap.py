b041d563da538f9c081a10614057ae4d5826a736
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-07-05 13:10:17 -0300

    Update binary gap solution

--- binary_gap/binary_gap.py
+++ binary_gap/binary_gap.py
@@ -1,11 +1,6 @@
-
-
-
-
-
-def solution(number):
+def n_binary_gap(number):
     """Returns the greatest binary gap on the number.
-    ex: The number 529 has binary representation 1000010001 and contains 
+    ex: The number 529 has binary representation 1000010001 and contains
     two binary gaps: one of length 4 and one of length 3.
 
     Complexity: O(n).
@@ -15,13 +10,13 @@ def solution(number):
     Returns:
         Length of the greatest gap or 0.
     """
-    bin_repr = format(number, 'b')
+    bin_repr = format(number, "b")
     size = len(bin_repr)
-    
+
     for n in range(size - 1):
-        if bin_repr[n] == '1':
-            zeroes = bin_repr[n:].split('1')
+        if bin_repr[n] == "1":
+            zeroes = bin_repr[n:].split("1")
             if len(zeroes) > 2:
-                return (len(max(zeroes)))
+                return len(max(zeroes))
             break
     return 0
