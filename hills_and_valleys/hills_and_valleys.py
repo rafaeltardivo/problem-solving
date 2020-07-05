c63d8585dc011885d5deeebb69dac8174ca1c9db
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-07-05 19:56:38 -0300

    Add hills and valleys complexity

--- hills_and_valleys/hills_and_valleys.py
+++ hills_and_valleys/hills_and_valleys.py
@@ -5,6 +5,8 @@ def solution(n):
     - If one of the neighbors is an edge, the condition is automatically
     satisfied.
 
+    Complexity: O(N^2).
+
     Args:
         n (list): list of numbers.
     Returns:
@@ -16,13 +18,13 @@ def solution(n):
     # edge case: empty list
     if not size:
         return 0
-    
+
     # edge case: only one item or all elements are equal
     if size == 1 or n.count(n[0]) == size:
         return 1
 
     i = 0
-    while(i < size):
+    while i < size:
         previous = None if i == 0 else n[i - 1]
         current = n[i]
 
@@ -37,7 +39,7 @@ def solution(n):
             i += 1
             current = nex
             nex = n[i + 1]
-        
+
         if not previous or not nex:
             hills_and_valleys += 1
         elif previous < current > nex or previous > current < nex:
@@ -45,20 +47,15 @@ def solution(n):
         i += 1
     return hills_and_valleys
 
-  
-if __name__ == '__main__':
-    print(solution([1]))                   # 1
-    print(solution([1,2]))                 # 2
-    print(solution([1,1,1]))               # 1
-    print(solution([1,2,1]))               # 3
-    print(solution([1,2,6]))               # 2
-    print(solution([1,2,3,4,4,3,4,4,5,6])) # 4
-    print(solution([-10,2,2,2,2]))         # 2
-    print(solution([1,0,0,0,1]))           # 3
-    print(solution([0,1,0,1,0]))           # 5
-    print(solution([]))                    # 0
 
-
-
-
-
+if __name__ == "__main__":
+    print(solution([1]))  # 1
+    print(solution([1, 2]))  # 2
+    print(solution([1, 1, 1]))  # 1
+    print(solution([1, 2, 1]))  # 3
+    print(solution([1, 2, 6]))  # 2
+    print(solution([1, 2, 3, 4, 4, 3, 4, 4, 5, 6]))  # 4
+    print(solution([-10, 2, 2, 2, 2]))  # 2
+    print(solution([1, 0, 0, 0, 1]))  # 3
+    print(solution([0, 1, 0, 1, 0]))  # 5
+    print(solution([]))  # 0
